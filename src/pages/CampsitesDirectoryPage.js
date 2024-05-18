102d22f4c192a52d67afda115669c6d6f4a1d984
J-Santoyo <joanasantoyo030@gmail.com>
2024-05-18 11:12:15 -0600

    Breadcrumbs SubHeader

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,12 @@
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
     // const [selectedCampsite, toggleCampsite] = useState(selectRandomCampsite())
     return (
         <Container>
+            <SubHeader current='Directory' />
             <CampsitesList />
         </Container>
     );
