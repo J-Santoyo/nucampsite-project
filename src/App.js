3a25fa2528cfe64e17e3dc6b054d826a65ae44d7
J-Santoyo <joanasantoyo030@gmail.com>
2024-05-19 16:51:42 -0600

    added partners list to aboutpage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 // import CampsiteCard from './features/campsites/CampsiteCard.js';
 // import { CAMPSITES } from './app/shared/CAMPSITES';
@@ -24,6 +25,7 @@ function App() {
         <Route path='/contact' element={<ContactPage />} /> /
         <Route path='/directory' element={<CampsitesDirectoryPage />} /> /
         <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        <Route path='/about' element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
