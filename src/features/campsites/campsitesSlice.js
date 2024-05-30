98f67139cd15f6bd853e1da537b397cfe80b14cf
J-Santoyo <joanasantoyo030@gmail.com>
2024-05-30 11:25:36 -0600

    Campsites useSelector

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,7 +1,7 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState {
+const initialState = {
     campsitesArray: CAMPSITES
 };
 
@@ -12,18 +12,18 @@ const campsitesSlice = createSlice({
 
 export const campsitesReducer = campsitesSlice.reducer;
 
-export const selectAllCampsites = () => {
-    return CAMPSITES;
+export const selectAllCampsites = (state) => {
+    return state.campsites.campsitesArray;
 };
 
 // export const selectRandomCampsite = () => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 // };
 
-export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+export const selectCampsiteById = (id) => (state) => {
+    return state.campsites.campsitesArray.find((campsite) => campsite.id === parseInt(id));
 };
 
-export const selectFeaturedCampsite = () => {
-    return CAMPSITES.find((campsite) => campsite.featured);
+export const selectFeaturedCampsite = (state) => {
+    return state.campsites.campsitesArray.find((campsite) => campsite.featured);
 }
