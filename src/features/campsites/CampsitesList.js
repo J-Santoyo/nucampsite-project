fd18fbc35e79191d7876b1ca674f9e039f72cb25
J-Santoyo <joanasantoyo030@gmail.com>
2024-06-10 19:40:19 -0600

    fixed errors with old CAMPSITES data

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { CAMPSITES } from '../../app/shared/oldData/CAMPSITES';
+// import { CAMPSITES } from '../../app/shared/oldData/CAMPSITES';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
@@ -28,7 +28,7 @@ const CampsitesList = () => {
     }
     return (
         <Row className="ms-auto">
-            {CAMPSITES.map((campsite) => {
+            {campsites.map((campsite) => {
                 return (
                     <Col md='5' className='m-4' key={campsite.id}>
                         <CampsiteCard campsite={campsite} />
